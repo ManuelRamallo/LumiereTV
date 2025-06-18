c7ed1a441085d95e101207ea8c01adf8e7fec87a
Manuel Ramallo Diaz <mramallodiaz96@gmail.com>
2025-06-18 19:44:23 +0200

    Init project for LumiereTV

---
+++ app/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("kotlin-parcelize")
+}
+
+android {
+    namespace = "com.mramallo.lumieretv"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.mramallo.lumieretv"
+        minSdk = 21
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.leanback)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.glide)
+    implementation(libs.androidx.lifecycle)
+    implementation(libs.okHttp3)
+    implementation(libs.retrofit2)
+    implementation(libs.converter.gson)
+    implementation(libs.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.fragment)
+}
