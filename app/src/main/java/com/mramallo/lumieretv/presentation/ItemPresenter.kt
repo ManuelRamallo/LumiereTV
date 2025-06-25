18c1903b4aeeba313ee9534f2f07b862c0377b8d
Manuel Ramallo Diaz <mramallodiaz96@gmail.com>
2025-06-25 18:00:24 +0200

    FEATURE: WIP draw home and list movies

---
+++ app/src/main/java/com/mramallo/lumieretv/presentation/ItemPresenter.kt
@@ -0,0 +1,78 @@
+package com.mramallo.lumieretv.presentation
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.leanback.widget.Presenter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import com.mramallo.lumieretv.R
+import com.mramallo.lumieretv.data.Detail
+
+class ItemPresenter: Presenter() {
+    override fun onCreateViewHolder(parent: ViewGroup): ViewHolder {
+
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_view, parent, false)
+
+        val params = view.layoutParams
+        params.width = getWidthInPercent(parent.context, 12)
+        params.height = getHeightInPercent(parent.context, 32)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, item: Any?) {
+        val content = item as? Detail
+        Log.d("image", "content: $content")
+
+        val imageView = viewHolder.view.findViewById<ImageView>(R.id.poster_image)
+
+        val url = "https://www.themoviedb.org/t/p/w500" + content?.poster_path
+        Log.d("image", "URL: $url")
+        Glide.with(viewHolder.view.context)
+            .load(url)
+            .addListener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    Log.e("Glide", "Error al cargar la imagen: ${e?.message}")
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable,
+                    model: Any,
+                    target: com.bumptech.glide.request.target.Target<Drawable>?,
+                    dataSource: DataSource,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    Log.d("Glide", "Imagen cargada correctamente")
+                    return false
+                }
+            })
+            .into(imageView)
+
+    }
+
+    override fun onUnbindViewHolder(viewHolder: ViewHolder) {}
+
+    private fun getWidthInPercent(context: Context, percent: Int): Int {
+        val width = context.resources.displayMetrics.widthPixels ?: 0
+        return (width * percent) / 100
+    }
+
+    private fun getHeightInPercent(context: Context, percent: Int): Int {
+        val height = context.resources.displayMetrics.heightPixels ?: 0
+        return (height * percent) / 100
+    }
+}
--- app/src/main/java/com/mramallo/lumieretv/presentation/MainActivity.kt
+++ app/src/main/java/com/mramallo/lumieretv/presentation/MainActivity.kt
@@ -1,15 +1,22 @@
-package com.mramallo.lumieretv
+package com.mramallo.lumieretv.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.FragmentActivity
+import com.google.gson.Gson
+import com.mramallo.lumieretv.R
+import com.mramallo.lumieretv.data.DataModel
 import com.mramallo.lumieretv.databinding.ActivityMainBinding
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
 
 class MainActivity : FragmentActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var listFragment: ListFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +30,18 @@ class MainActivity : FragmentActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Load fragment for list movies
+        listFragment = ListFragment()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.list_fragment, listFragment)
+        transaction.commit()
+
+        val gson = Gson()
+        val i: InputStream = this.assets.open("movies.json")
+        val br = BufferedReader(InputStreamReader(i))
+        val dataList: DataModel = gson.fromJson(br, DataModel::class.java)
+
+        listFragment.binData(dataList = dataList)
     }
 }
