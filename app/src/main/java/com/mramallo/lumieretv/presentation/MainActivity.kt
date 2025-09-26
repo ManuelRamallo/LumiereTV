21d91f142bf2b96f0c8d5aace8033aac30874a4b
Manuel Ramallo Diaz <mramallodiaz96@gmail.com>
2025-09-26 18:47:49 +0200

    FEATURE: Add navigation bar and icons

---
+++ app/src/main/java/com/mramallo/lumieretv/presentation/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.mramallo.lumieretv.presentation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentActivity
+import com.mramallo.lumieretv.R
+import com.mramallo.lumieretv.databinding.ActivityMainBinding
+
+
+class MainActivity : FragmentActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+    }
+}
