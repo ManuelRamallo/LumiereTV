77826f6bcf484fbb14507481b16309094f20dfd9
Manuel Ramallo Diaz <mramallodiaz96@gmail.com>
2025-10-22 19:03:27 +0200

    Refactor: Improve side menu UI and fix potential NPE
    Remove the unnecessary null-safe operator (`?: 0`) in `getWidthInPercent` as `widthPixels` is not nullable.
    Update the side menu `TextViews` in `activity_main.xml` to include text labels, drawable padding, and vertical gravity for better alignment and usability.

--- app/src/main/java/com/mramallo/lumieretv/util/Common.kt
+++ app/src/main/java/com/mramallo/lumieretv/util/Common.kt
@@ -3,6 +3,6 @@ package com.mramallo.lumieretv.util
 import android.content.Context
 
 fun getWidthInPercent(context: Context, percent: Int): Int {
-    val width = context.resources.displayMetrics.widthPixels ?: 0
+    val width = context.resources.displayMetrics.widthPixels
     return (width * percent) / 100
 }
