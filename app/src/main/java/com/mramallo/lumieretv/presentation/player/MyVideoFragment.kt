7074cb056fab4770adc71e3d1b52e551c61d028f
Manuel Ramallo Diaz <mramallodiaz96@gmail.com>
2025-11-11 18:32:56 +0100

    FEATURE: Add fast-forward and rewind functionality to the player
    Implement custom transport controls to handle fast-forward and rewind actions. This includes adding new icons, layouts for visual feedback, and a custom `PlaybackTransportControlGlue` to manage the player's state and UI.
    
    Add animation for the fast-forward and rewind indicators.
    Introduce a `BasicMediaPlayerAdapter` to override seeking behavior.

--- app/src/main/java/com/mramallo/lumieretv/presentation/player/MyVideoFragment.kt
+++ app/src/main/java/com/mramallo/lumieretv/presentation/player/MyVideoFragment.kt
@@ -2,6 +2,11 @@ package com.mramallo.lumieretv.presentation.player
 
 import android.net.Uri
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.leanback.app.VideoSupportFragment
 import androidx.leanback.app.VideoSupportFragmentGlueHost
 import androidx.leanback.media.MediaPlayerAdapter
@@ -9,10 +14,17 @@ import androidx.leanback.media.PlaybackGlue
 import androidx.leanback.media.PlaybackTransportControlGlue
 import androidx.leanback.widget.PlaybackSeekDataProvider
 import androidx.core.net.toUri
+import androidx.core.view.isVisible
+import com.mramallo.lumieretv.R
 
 class MyVideoFragment: VideoSupportFragment() {
 
+    private lateinit var transportGlue: CustomTransportControlGlue
+
     private lateinit var playerGlue: PlaybackTransportControlGlue<MediaPlayerAdapter>
+
+    private lateinit var fastForwardIndicatorView: View
+    private lateinit var rewindIndicatorView: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +33,7 @@ class MyVideoFragment: VideoSupportFragment() {
             setRepeatAction(PlaybackTransportControlGlue.ACTION_REPEAT)
         }
 
-        playerGlue = PlaybackTransportControlGlue(requireActivity(), playerAdapter).apply {
+        /*playerGlue = PlaybackTransportControlGlue(requireActivity(), playerAdapter).apply {
             host = VideoSupportFragmentGlueHost(this@MyVideoFragment)
             title = "My Android TV Development"
             subtitle = "My demo Subtitle"
@@ -36,7 +48,75 @@ class MyVideoFragment: VideoSupportFragment() {
                     playerGlue.play()
                 }
             }
-        })
+        })*/
+
+        transportGlue = CustomTransportControlGlue(
+            context = requireContext(),
+            playerAdapter = BasicMediaPlayerAdapter(requireContext())
+        )
+
+        transportGlue.host = VideoSupportFragmentGlueHost(this)
+        transportGlue.subtitle = "My demo subtitle"
+        transportGlue.title = "My Android TV Development"
+        val uriPath = "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+        transportGlue.playerAdapter.setDataSource(Uri.parse(uriPath))
+
+        setOnKeyInterceptListener { view,  keyCode, event ->
+            if( isControlsOverlayVisible || event.repeatCount > 0) {
+                isShowOrHideControlsOverlayOnUserInteraction  = true
+            } else when(keyCode) {
+                KeyEvent.KEYCODE_DPAD_RIGHT -> {
+                    isShowOrHideControlsOverlayOnUserInteraction = event.action != KeyEvent.ACTION_DOWN
+                    if(event.action == KeyEvent.ACTION_DOWN) {
+                        animateIndicator(fastForwardIndicatorView)
+                    }
+                }
+                KeyEvent.KEYCODE_DPAD_LEFT -> {
+                    isShowOrHideControlsOverlayOnUserInteraction = event.action != KeyEvent.ACTION_DOWN
+                    if(event.action == KeyEvent.ACTION_DOWN) {
+                        animateIndicator(rewindIndicatorView)
+                    }
+                }
+            }
+            transportGlue.onKey(view, keyCode, event)
+        }
     }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = super.onCreateView(inflater, container, savedInstanceState) as ViewGroup
+
+        fastForwardIndicatorView = inflater.inflate(R.layout.view_forward, view, false)
+        view.addView(fastForwardIndicatorView)
+
+        rewindIndicatorView = inflater.inflate(R.layout.view_rewind, view, false)
+        view.addView(rewindIndicatorView)
+
+
+        return view
+    }
+
+    private fun animateIndicator(indicator: View) {
+        indicator.animate()
+            .withEndAction {
+                indicator.isVisible = false
+                indicator.alpha = 1F
+                indicator.scaleX = 1F
+                indicator.scaleY = 1F
+            }
+            .withStartAction {
+                indicator.isVisible = true
+            }
+            .alpha(0.2F)
+            .scaleX(2F)
+            .scaleY(2F)
+            .setDuration(400)
+            .setInterpolator(AccelerateDecelerateInterpolator())
+            .start()
+    }
+
 
 }
