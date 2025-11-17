d1099cc03c5e3fb9815e3ec32f9f93dc1dbf17a7
Manuel Ramallo Diaz <mramallodiaz96@gmail.com>
2025-11-17 18:47:25 +0100

    FEATURE: Add movie list fetching
    Add data models (`DataModelResponse`, `Result`, `Dates`) to handle the movie list API response.
    Update `ApiService` to include endpoints for "now_playing" and "top_rated" movies.
    Implement repository methods in `TmdbRepo` to fetch and expose "now_playing" and "top_rated" movie lists as `LiveData`.

--- app/src/main/java/com/mramallo/lumieretv/data/api/ApiService.kt
+++ app/src/main/java/com/mramallo/lumieretv/data/api/ApiService.kt
@@ -1,6 +1,7 @@
 package com.mramallo.lumieretv.data.api
 
 import com.mramallo.lumieretv.data.model.CastResponse
+import com.mramallo.lumieretv.data.model.DataModelResponse
 import com.mramallo.lumieretv.data.model.DetailResponse
 import retrofit2.Response
 import retrofit2.http.GET
@@ -8,6 +9,17 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
+
+    @GET("movie/now_playing?language=en-US&page=1")
+    suspend fun getNowPlayingList(
+        @Query("api_key") apiKey: String
+    ): Response<DataModelResponse>
+
+    @GET("movie/top_rated?language=en-US&page=1")
+    suspend fun getTopRatedList(
+        @Query("api_key") apiKey: String
+    ): Response<DataModelResponse>
+
     @GET("movie/{movie_id}")
     suspend fun getMovieDetails(
         @Path("movie_id") movieId: Int,
---
+++ app/src/main/java/com/mramallo/lumieretv/data/model/Dates.kt
@@ -0,0 +1,6 @@
+package com.mramallo.lumieretv.data.model
+
+data class Dates(
+    val maximum: String,
+    val minimum: String
+)
