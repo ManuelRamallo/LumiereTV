b2ab8df20de6f6e7c2e540d9430c30f898378a55
Manuel Ramallo Diaz <mramallodiaz96@gmail.com>
2025-11-17 18:50:40 +0100

    FEATURE: Create HomeViewModel
    Create a `HomeViewModel` to fetch now playing and top-rated movies from the repository.
    The ViewModel exposes the movie data as `LiveData` for the UI to observe.

---
+++ app/src/main/java/com/mramallo/lumieretv/presentation/HomeViewModel.kt
@@ -0,0 +1,27 @@
+package com.mramallo.lumieretv.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mramallo.lumieretv.data.api.Response
+import com.mramallo.lumieretv.data.api.TmdbRepo
+import com.mramallo.lumieretv.data.model.DataModelResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class HomeViewModel(val repo: TmdbRepo) : ViewModel() {
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            repo.getNowPlayingMovies()
+            repo.getTopRatedMovies()
+        }
+    }
+
+    val nowPlayingMovies: LiveData<Response<DataModelResponse>>
+        get() = repo.nowPlayingMovies
+
+    val topRatedMovies: LiveData<Response<DataModelResponse>>
+        get() = repo.topRatedMovies
+
+}
